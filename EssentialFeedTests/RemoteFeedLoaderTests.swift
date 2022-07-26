861bcf0e7f4345fd8e0488a1489954feafa5b0d5
Pawel Klapuch <pawelklapuch@gmail.com>
2022-07-25 20:13:52 -0500

    [DEV] HTTP Client (Singleton))

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,17 +8,30 @@
 import XCTest
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "http://google.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    
     var requestedURL: URL?
+    
+    private init() { }
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_init_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        sut.load()
+        XCTAssertNotNil(client.requestedURL)
     }
 }
