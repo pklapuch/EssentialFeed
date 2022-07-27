cc17fc52684965766a5ba739b837f2933eae979c
Pawel Klapuch <pawelklapuch@gmail.com>
2022-07-26 20:55:05 -0500

    user explicit json data for non 200 http response tests

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -38,12 +38,10 @@ public final class RemoteFeedLoader {
     public func load(completion:@escaping (Result) -> Void) {
         client.getFrom(from: url) { result in
             switch result {
-            case .success(let data, _):
-                do {
-                    let root = try JSONDecoder().decode(Root.self, from: data)
+            case .success(let data, let response):
+                if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
                     completion(.success(root.items))
-                } catch {
-                    print(error)
+                } else {
                     completion(.failure(.invalidData))
                 }
             case .failure:
