55ff8befe5e2599963dab80273ae5eb206a23146
Pawel Klapuch <pawelklapuch@gmail.com>
2022-07-26 22:15:57 -0500

    moved http client to its own file

---
+++ EssentialFeed/Feed API/HTTPClient.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPClient.swift
+//  EssentialFeed
+//
+//  Created by Pawel Klapuch on 7/26/22.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func getFrom(from url: URL, completion:@escaping (HTTPClientResult) -> Void)
+}
