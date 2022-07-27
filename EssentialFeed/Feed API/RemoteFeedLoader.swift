f6c709d2b59a3dd07b14e5111d2b808a08e95834
Pawel Klapuch <pawelklapuch@gmail.com>
2022-07-26 22:40:27 -0500

    guarantee we do not deliver a result after remote feed load has been deallocated

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -27,7 +27,9 @@ public final class RemoteFeedLoader {
     }
     
     public func load(completion:@escaping (Result) -> Void) {
-        client.getFrom(from: url) { result in
+        client.getFrom(from: url) { [weak self] result in
+            guard self != nil else { return }
+            
             switch result {
             case .success(let data, let response):
                 completion(FeedItemsMapper.map(data, from: response))
@@ -36,6 +38,4 @@ public final class RemoteFeedLoader {
             }
         }
     }
-    
-
 }
--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -77,6 +77,20 @@ final class RemoteFeedLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
+        let url = URL(string: "https://any.com")!
+        let client = HTTPClientSpy()
+        var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
+        
+        var capturedResults = [RemoteFeedLoader.Result]()
+        sut?.load { capturedResults.append($0) }
+        
+        sut = nil
+        client.complete(withStatusCode: 200, data: makeItemsJSON([]))
+        
+        XCTAssertTrue(capturedResults.isEmpty)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://google.com")!,
